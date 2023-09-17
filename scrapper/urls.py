c2c93fb7cff5e60ba78c222e7013a5642dfe031a
Mohd Tehami <officialtehami@gmail.com>
2023-09-17 19:38:19 +0530

    mongodb implememnted
    Signed-off-by: Mohd Tehami <officialtehami@gmail.com>

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # ... Other URL patterns ...
-
+    
     # URL pattern for scraping properties
     path('scrape/', views.scrape_properties, name='scrape_properties'),
 
     # URL pattern for listing properties by city
     path('property_list/<str:city>/', views.property_list, name='property_list'),
+    path('', RedirectView.as_view(url='/scrape/')),
 ]
